7f3810957003f85c11e4aa7fdd802f14a8d19bdd
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-03-15 18:58:37 +0000

    updated context processor and start shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,12 +1,27 @@
+"""contexts allows functions to be seen across all templates"""
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Products
 
 
 def bag_contents(request):
+    """view the bag contents"""
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Products, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
