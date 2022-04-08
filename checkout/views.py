0d45afafa75af1779b2bbd987b15970fb3e552d9
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-04-08 14:12:48 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KmI7nEliIrUhfhTsSRBAClkhpQg2DWTHBntfefm03IEul5Kh1ycjtkDE40wjAP17vJ17e5c1eugRBOi1vNAUgrE00oAyyxGYq',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
